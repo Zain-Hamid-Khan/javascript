f0ef04372d921991aca30fef67102ef2dff0294f
Zain Khan <109334923+Zain-Hamid-Khan@users.noreply.github.com>
2025-04-15 09:22:35 +0000

    javascript day 2

---
+++ 01_basics/05_moreStrings.js
@@ -0,0 +1,33 @@
+const name="Zain"
+const repo="10"
+
+console.log("Hello" +   name +   repo)
+
+console.log(`Hello my name is ${name} and my repo count is ${repo}`);
+
+const gameName = "Hamid-Khan"
+
+console.log(gameName);
+
+console.log(gameName[3]);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("d"));
+
+const newString = gameName.substring(0,3)
+console.log(newString)
+
+const anotherString = gameName.slice(6 ,10)
+console.log(anotherString);
+
+const anotherString2 =  "   zzaaiinn   "
+console.log(anotherString2)
+
+console.log(anotherString2.trim())
+
+const anotherString3 = "Hello-World-Zain"
+console.log(anotherString3.split("-"))
+
+
+
+
---
+++ 01_basics/06_numbers.js
@@ -0,0 +1,37 @@
+const number = 200
+const number2 = new Number(300)
+
+console.log(number);
+console.log(number2);
+
+console.log(numInString = number.toString().length)
+console.log(number.toFixed(3));
+
+const number3 = 118.545
+
+console.log(number3.toPrecision(5))
+
+const number4 = 10000000
+console.log(number4.toLocaleString('en-IN'));
+
+
+// ----------------- MATHS ---------------------//
+
+console.log(Math);
+console.log(Math.abs(3.6));
+console.log(Math.floor(3.66));
+console.log(Math.random());
+console.log(Math.round(3.66));
+console.log(Math.sqrt(9));
+
+let min=10
+let max=20
+
+console.log (Math.floor(Math.random() * (max - min) + min))
+
+
+
+
+
+
+
