dd7d7778275cff126c03008a29b085e8fdccadf3
Zain Khan <109334923+Zain-Hamid-Khan@users.noreply.github.com>
2025-05-02 17:14:59 +0000

    javascript day 6

--- 02_basics/02_Objects.js
+++ 02_basics/02_Objects.js
@@ -1,25 +1,2 @@
 
-let sym = Symbol("key1")
-
-const Object1 = {
-    name: "zain",
-    "full name": "Hitesh Chaudhary",
-    age: 22,
-    isMale: true,
-    [sym]: "newkey1",
-}
-
-console.log(Object1["full name"]);
-console.log(typeof sym);
-
-console.log(Object[sym]);
-
-console.log(Object1);
-
-Object1.greeting = function()
-{
-    console.log(`hello user, ${this.name}`) ;
-}
-
-console.log(Object1.greeting());
-
+?
